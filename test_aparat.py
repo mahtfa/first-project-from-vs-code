0266bee5ce33a5c5d4a85be7413b56f4bc49d4b8
mahtfa <mohammadhatefi1234@gmail.com>
2025-11-23 23:39:14 +0330

    first

---
+++ test_aparat.py
@@ -0,0 +1,22 @@
+from aparat import Aparat
+
+username = "_TBMG_"
+password = "mahmahk81"
+
+aparat = Aparat()
+
+user = aparat.login(username, password)
+form = aparat.uploadForm(user.username, user.ltoken)
+
+video = aparat.uploadPost(
+    form=form,
+    video_path='video.mp4',
+    title='title',
+    category=3,
+    tags=['english', 'learn english', 'teaching'],
+    allow_comment=True,
+    descreption='',
+    video_pass=False
+)
+
+print(video)
